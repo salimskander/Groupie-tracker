8db06150afc9b83cd1768cc3cc879517675ab5aa
mmederic <mederic.marquie@ynov.com>
2022-12-15 15:54:06 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello, World!</h1>")
+
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8000", nil)
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
 }

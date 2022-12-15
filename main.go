9c685ef75026e3ff8d9984de9ef5a07c48a23340
mmederic <mederic.marquie@ynov.com>
2022-12-15 16:48:02 +0100

    Server en fonction

--- main.go
+++ main.go
@@ -8,7 +8,16 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello, World!</h1>")
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
 
+	if r.Method != "GET" {
+		http.Error(w, "Method not supported.", http.StatusNotFound)
+		return
+	}
+	fmt.Printf("Everything is good\n")
 }
 
 func main() {
